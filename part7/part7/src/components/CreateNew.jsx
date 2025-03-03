a603fde2ba5e2cc519b191a034dcb1968dd7f1f1
Thiago Laurence <francolaurence47@gmail.com>
2025-03-03 14:45:29 -0300

    exercise(anecdotes): implementacion de ejercicio 7.4 - 7.6

--- part7/part7/src/components/CreateNew.jsx
+++ part7/part7/src/components/CreateNew.jsx
@@ -1,41 +1,46 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks/index'
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+    const [content, resetContent] = useField('text')
+    const [author, resetAuthor] = useField('text')
+    const [info, resetInfo] = useField('text')
     const navigate = useNavigate()
   
     const handleSubmit = (e) => {
       e.preventDefault()
       props.addNew({
-        content,
-        author,
-        info,
+        content: content.value,
+        author: author.value,
+        info: info.value,
         votes: 0
       })
-      navigate('/', { state: { message: `A new anecdote "${content}" has been created!` } })
+      navigate('/', { state: { message: `A new anecdote "${content.value}" has been created!` } })
+    }
+
+    const resetForm = (fields) => {
+      fields.forEach(field => field.reset())
     }
   
     return (
       <div>
-        <h2>create a new anecdote</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
-          </div>
-          <div>
-            author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
-          </div>
-          <div>
-            url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
-          </div>
-          <button>create</button>
-        </form>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+        content
+        <input name='content' {...content} />
+        </div>
+        <div>
+        author
+        <input name='author' {...author} />
+        </div>
+        <div>
+        url for more info
+        <input name='info' {...info} />
+        </div>
+        <button type="submit">create</button>
+        <button type="button" onClick={() => resetForm([resetContent, resetAuthor, resetInfo])}>reset</button>
+      </form>
       </div>
     )
   
