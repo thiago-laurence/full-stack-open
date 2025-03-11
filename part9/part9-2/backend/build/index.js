f260560977842678613745d57682545dcc00ed00
Thiago Laurence <francolaurence47@gmail.com>
2025-03-11 18:11:56 -0300

    exercise(typescript-backend): implementacion de ejercicio 9.9

--- part9/part9-2/backend/build/index.js
+++ part9/part9-2/backend/build/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+const cors_1 = __importDefault(require("cors"));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 const PORT = 3000;
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
--- part9/part9-2/backend/index.ts
+++ part9/part9-2/backend/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 const app = express();
+
+import cors from 'cors';
+app.use(cors());
+
 app.use(express.json());
 
 const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
